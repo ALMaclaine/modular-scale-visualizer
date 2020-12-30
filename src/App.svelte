2171010716c3a57abb3d2ef7ca484fd5f0311b62
ALMaclaine <almaclaine@gmail.com>
2020-12-29 23:32:55 -0600

    add code

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,34 @@
 <script>
-	export let name;
+	import {MusicalRatios, ratioToPower} from 'musical-ratios';
+	import {MediaQueryManager} from 'media-query-manager';
+	const BaseViewWidth = 1280;
+	let ratioSelection = '';
+	let bpValues = [];
+
+	$: {
+		const ratio = MusicalRatios[ratioSelection];
+		const newBreaks = [-3, -2, -1, 0, 1, 2].map(e => BaseViewWidth * ratioToPower(ratio, e));
+		const mediaManager = new MediaQueryManager(newBreaks);
+		bpValues = mediaManager.breaks;
+	}
+
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<select bind:value={ratioSelection}>
+	{#each Object.keys(MusicalRatios).filter(e => isNaN(parseFloat(e))) as rat}
+		<option value={rat}>
+			{rat}
+		</option>
+	{/each}
+</select>
+
+<ol>
+	{#each bpValues as bp}
+		<li>{bp}</li>
+	{/each}
+</ol>
