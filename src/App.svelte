a11a548f58c26ccd1835453f11bcc8174dc2a7ac
ALMaclaine <almaclaine@gmail.com>
2020-12-30 00:30:59 -0600

    finish basic break point explorer

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,84 @@
 <script>
 	import {MusicalRatios, ratioToPower} from 'musical-ratios';
 	import {MediaQueryManager} from 'media-query-manager';
-	const BaseViewWidth = 1280;
-	let ratioSelection = '';
-	let bpValues = [];
+	let baseViewWidth = 1280;
+	let ratioSelection = MusicalRatios[MusicalRatios.PerfectFifth];
+	let bpValues;
+	let lowBpRange = -3;
+	let highBprange = 3;
+
+	const range = (start, end) => {
+		const length = end - start;
+		return Array.from({ length }, (_, i) => start + i);
+	}
+
+	let mediaManager = new MediaQueryManager(range(lowBpRange, highBprange).map(e => baseViewWidth * ratioToPower(MusicalRatios.PerfectFifth, e)));
+	let active = mediaManager.active;
 
 	$: {
 		const ratio = MusicalRatios[ratioSelection];
-		const newBreaks = [-3, -2, -1, 0, 1, 2].map(e => BaseViewWidth * ratioToPower(ratio, e));
-		const mediaManager = new MediaQueryManager(newBreaks);
+		const newBreaks = range(lowBpRange, highBprange).map(e => baseViewWidth * ratioToPower(ratio, e));
+		mediaManager = new MediaQueryManager(newBreaks);
+
+		mediaManager.addEventListener('change', () => {
+			active = mediaManager.active;
+		});
+
 		bpValues = mediaManager.breaks;
 	}
-
 
 </script>
 
 <style>
+	nav {
+		width: 100%;
+		position: absolute;
+		top: 0;
+		display: flex;
+	}
 
+	.bp-info {
+		position: absolute;
+		bottom: 10px;
+		left: 10px;
+		background: rgba(0, 0, 0, .5);
+		border-radius: 4px;
+		padding: 32px;
+	}
+
+	.selected {
+		color: red;
+	}
 </style>
 
-<select bind:value={ratioSelection}>
-	{#each Object.keys(MusicalRatios).filter(e => isNaN(parseFloat(e))) as rat}
-		<option value={rat}>
-			{rat}
-		</option>
-	{/each}
-</select>
+<nav>
+	<select bind:value={ratioSelection}>
+		{#each Object.keys(MusicalRatios).filter(e => isNaN(parseFloat(e))) as rat}
+			<option value={rat}>
+				{rat}
+			</option>
+		{/each}
+	</select>
+	<label>
+		Low BP
+		<input type=number bind:value={lowBpRange} min=-8 max=0>
+		<input type=range bind:value={lowBpRange} min=-8 max=0>
+	</label>
+	<label>
+		High BP
+		<input type=number bind:value={highBprange} min=0 max=8>
+		<input type=range bind:value={highBprange} min=0 max=8>
+	</label>
+	<label>
+		BaseViewWidth
+		<input type=number bind:value={baseViewWidth} min=640 max=2560>
+	</label>
+</nav>
 
-<ol>
-	{#each bpValues as bp}
-		<li>{bp}</li>
-	{/each}
-</ol>
+<div class='bp-info'>
+	<ol start={lowBpRange}>
+		{#each bpValues as bp}
+			<li class="{bp === active ? 'selected' : ''}">{bp}</li>
+		{/each}
+	</ol>
+</div>
